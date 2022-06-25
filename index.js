c34d7255935f6d0a96044181b7161261b1b98d2f
Houndoom <therealhoundoom@gmail.com>
2022-06-25 14:48:12 +0800

    Add plus minus function and redesign screen to fit plus minus sign nicely

--- index.js
+++ index.js
@@ -20,13 +20,13 @@ function operate(func,a,b) {
 
 /* Set up LCD screen */
 
-const numDigits = 14;
+const numDigits = 13;
 let decimalPressed = false;
 let decimalExists = false;
 
 const screen = document.getElementById('screen');
 
-for (let i = 1; i <= numDigits*2 - 1; i++) {
+for (let i = 1; i <= numDigits*2; i++) {
   const char = document.createElement('div');
   if (i % 2 === 1) {
     char.classList.add('digit');
@@ -39,21 +39,27 @@ for (let i = 1; i <= numDigits*2 - 1; i++) {
   screen.appendChild(char);
 }
 
+const char = document.createElement('div');
+char.classList.add('digit');
+char.setAttribute('id','minus-sign');
+screen.appendChild(char);
+
 /* Set up buttons */
 
 const allSymbols = ['MC','MR','M-','M+','\u00f7','+/-','7','8','9','\u00d7','C','4','5','6','-','AC','1','2','3','+','0','00','.','='];
 const buttons = document.getElementById('buttons');
+
+function convertUnicode(string) {
+  const stringArray = string.split('');
+  return stringArray.map(e => e.charCodeAt(0)).reduce((totalString,charCode) => totalString + charCode, 'u');
+}
 
 for (let i = 0; i < allSymbols.length; i++) {
   const symb = allSymbols[i];
   const buttonElement = document.createElement('button');
   buttonElement.classList.add(`col${i % 5}`);
   buttonElement.textContent = symb;
-  if (symb.length === 1) {
-    buttonElement.setAttribute('id',`u${symb.charCodeAt(0)}`);
-  } else {
-    buttonElement.setAttribute('id',`u${symb.charCodeAt(0)}${symb.charCodeAt(1)}`);
-  }
+  buttonElement.setAttribute('id',`${convertUnicode(symb)}`);
   buttons.appendChild(buttonElement);
 }
 
@@ -140,7 +146,7 @@ function inputDoubleZero() {
 /* Decimal point */
 
 const decimalPointButton = document.getElementById('u46');
-decimalPointButton.addEventListener('click',() => addDecimalPoint());
+decimalPointButton.addEventListener('click', () => addDecimalPoint());
 
 function addDecimalPoint() {
   if (decimalExists) return;
@@ -149,10 +155,22 @@ function addDecimalPoint() {
   return;
 }
 
+/* Minus sign */
+
+const minusSignButton = document.getElementById('u434745');
+minusSignButton.addEventListener('click', () => toggleMinusSign());
+
+function toggleMinusSign() {
+  const minusSign = document.getElementById('minus-sign');
+  if (!minusSign.textContent) minusSign.textContent = '-';
+  else minusSign.textContent = null;
+  return;
+}
+
 /* Clear buttons */
 
 const clearButton = document.getElementById('u67');
-clearButton.addEventListener('click',() => clear());
+clearButton.addEventListener('click', () => clear());
 
 function clear() {
   const allButtons = document.querySelectorAll('.digit, .decimal-point');
