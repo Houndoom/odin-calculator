7149f470fcce91318570c0d415afe587197fa818
Houndoom <therealhoundoom@gmail.com>
2022-06-25 11:56:50 +0800

    Set up LCD screen to have fixed digit and decimal point positions

--- index.js
+++ index.js
@@ -17,13 +17,66 @@ function divide(a,b) {
 function operate(func,a,b) {
   return func(a,b);
 }
-const allSymbols = ['MC','M+','\u00f7','\u00d7','7','8','9','-','4','5','6','+','1','2','3','=','0','.'];
+
+/* Set up LCD screen */
+
+const numDigits = 15;
+
+const screen = document.querySelector('#screen');
+
+for (let i = 1; i <= numDigits*2 - 1; i++) {
+  const char = document.createElement('div');
+  if (i % 2 === 1) {
+    char.classList.add('digit');
+    char.setAttribute('id',`digit${Math.ceil(i/2)}`);
+    if (i === numDigits*2 - 1) char.textContent = '0';
+  } else {
+    char.classList.add('decimal-point');
+    char.setAttribute('id',`point${i/2}`);
+  }
+  screen.appendChild(char);
+}
+/* Set up buttons */
+
+const allSymbols = ['MC','MR','M-','M+','\u00f7','+/-','7','8','9','\u00d7','C','4','5','6','-','AC','1','2','3','+','0','00','.','='];
 const buttons = document.querySelector('#buttons');
 
-for (const i of allSymbols) {
+for (let i = 0; i < allSymbols.length; i++) {
+  const symb = allSymbols[i];
   const buttonElement = document.createElement('button');
-  if (!['=','0'].includes(i)) buttonElement.classList.add('normal-button');
-  buttonElement.textContent = i;
-  buttonElement.setAttribute('id',`u${i.charCodeAt(0)}`);
+  buttonElement.classList.add(`col${i % 5}`);
+  buttonElement.textContent = symb;
+  if (symb.length === 1) {
+    buttonElement.setAttribute('id',`u${symb.charCodeAt(0)}`);
+  } else {
+    buttonElement.setAttribute('id',`u${symb.charCodeAt(0)}${symb.charCodeAt(1)}`);
+  }
   buttons.appendChild(buttonElement);
+}
+
+/* Input numbers */
+
+function maxDisplay() {
+  const lastDigit = document.querySelectorAll('.digit')[-1];
+  return (!!lastDigit.textContent);
+}
+
+for (let i = 48; i <= 57; i++) {
+  const button = document.querySelector(`#u${i}`);
+  button.addEventListener('click',inputNumber(numDigits));
+}
+
+function inputNumber(i) {
+  const digit = document.querySelector(`digit${i}`);
+  if (i === numDigits && !!digit) return;
+  if (!!digit) {
+    const nextDigit = document.querySelector(`digit${i+1}`);
+    if (i === 1) {
+      if (nextDigit.textContent || digit.textContent !== '0') nextDigit.textContent = digit.textContent;
+      digit.textContent === this.textContent;
+    } else {
+      document.querySelector(`digit${i+1}`).textContent = digit.textContent;
+    }
+  } 
+  if (i !== 1) inputNumber(i - 1);
 }
